c47983dcf7b20ecd13c37eb8a7ce0bc82788efe6
Aaron <abatilo@uccs.edu>
2015-03-13 09:29:27 -0600

    Collision and sticking works. Picked up items aren't colliding properly

---
+++ Assets/_Scripts/CollisionController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionController : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other) {
+		gameObject.AddComponent<FixedJoint> ();
+		gameObject.GetComponent<FixedJoint> ().connectedBody = other.rigidbody;
+		gameObject.rigidbody.isKinematic = false;
+		gameObject.collider.enabled = false;
+		gameObject.tag = "Player";
+	}
+}
