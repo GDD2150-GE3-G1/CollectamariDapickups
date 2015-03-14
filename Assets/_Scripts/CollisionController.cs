4eb41a923f9b9eba2c425396765c2693b73537e2
Aaron <abatilo@uccs.edu>
2015-03-14 15:02:46 -0600

    Messing with radius size change and force used to move the katamari

--- Assets/_Scripts/CollisionController.cs
+++ Assets/_Scripts/CollisionController.cs
@@ -5,6 +5,11 @@ public class CollisionController : MonoBehaviour {
 
 	void OnCollisionEnter(Collision info) {
 		if (info.gameObject.tag == "Pickup") {
+			rigidbody.mass += info.rigidbody.mass;
+
+			SphereCollider s = collider as SphereCollider;
+			s.radius += 0.02F;
+
 			Destroy (info.rigidbody);
 			info.transform.parent = transform;
 		}
--- Assets/_Spawners/CapsuleSpawner.cs
+++ Assets/_Spawners/CapsuleSpawner.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class CapsuleSpawner : MonoBehaviour {
 
+	public GameObject pickupObject;
+
 	private float timeElapsed = 0F;
-	private float spawnTime = 5F;
+	private float spawnTime = 0.5F;
+
 
 	void Update () {
 		timeElapsed += Time.deltaTime;
 
 		if (timeElapsed > spawnTime) {
-			Debug.Log("Time passed");
+			GameObject spawnedPickup = (GameObject)Instantiate(pickupObject);
+			spawnedPickup.transform.position = new Vector3(Random.Range(-25, 25), 1, Random.Range(-25, 25));
 			timeElapsed -= spawnTime;
 		}
 	}
