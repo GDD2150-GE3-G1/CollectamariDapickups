946219bdfaea3d8302ef2482aa2c7bdd0c589cdc
Aaron <abatilo@uccs.edu>
2015-03-31 06:19:35 -0600

    Modified .gitignore to ignore build files

--- Assets/_Scripts/WinCheckerScript.cs
+++ Assets/_Scripts/WinCheckerScript.cs
@@ -5,10 +5,8 @@ public class WinCheckerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		if (transform.position.y < 0) {
-			Application.Quit();
-		} else if (CollisionController.collectedPickups > 300) {
-			Application.Quit();
+		if (transform.position.y < 0 || CollisionController.collectedPickups > 300) {
+			Application.Quit ();
 		}
 	}
 }
